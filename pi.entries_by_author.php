53e64064d1c7c5f2a1ca26c9abb07b45783d1a8c
Andrew Reynolds <areynold@illinois.edu>
2019-07-03 11:37:06 -0500

    build article query

--- pi.entries_by_author.php
+++ pi.entries_by_author.php
@@ -56,26 +56,21 @@ class Entries_by_author
 
         $data_teaser_field = 'field_id_' . $this->required_fields['teaser'];
         $data_byline_field = 'field_id_' . $this->required_fields['byline'];
-        $screen_name_query = ee()->db->select('screen_name')
-            ->from('members')
-            ->where('member_id', $this->user);
-        // SET @data_teaser_field = CONCAT('field_id_', (SELECT field_id FROM exp_channel_fields WHERE field_name = 'Teaser'));
-        // SET @data_byline_field = CONCAT('field_id_', (SELECT field_id FROM exp_channel_fields WHERE field_name = 'Byline'));
-        // SET @screen_name_query = CONCAT('(SELECT members.screen_name FROM exp_members members WHERE members.member_id = ', @user_id, ')');
-        // SET @byline_query = CONCAT('
-		        // SELECT titles.entry_id, titles.title, titles.entry_date, titles.url_title, data.', @data_teaser_field, ', data.', @data_byline_field, ' 
-                // FROM exp_channel_data data 
-                // INNER JOIN exp_channel_titles titles ON data.entry_id = titles.entry_id
-                // INNER JOIN exp_members members ON
-			        // LOCATE(
-				        // (SELECT members.screen_name FROM exp_members members WHERE members.member_id = ?),',
-                        // @data_byline_field, '
-                    // ) > 0
-                    // GROUP BY entry_id
-            // ');
-        // PREPARE statement from @byline_query;
-        // SET @a = '11';
-        // EXECUTE statement USING @a;
-        // DEALLOCATE PREPARE statement;
+        $screen_name_query = "SELECT screen_name from {$this->db_prefix}members WHERE member_id = ?";
+
+        $byline_query = 
+            "SELECT titles.entry_id, titles.title, titles.entry_date, titles.url_title, data.{$data_teaser_field}, data.{$data_byline_field}
+                FROM {$this->db_prefix}channel_data data
+                INNER JOIN {$this->db_prefix}channel_titles titles ON data.entry_id = titles.entry_id
+                INNER JOIN {$this->db_prefix}members members ON
+                    LOCATE(
+                        ({$screen_name_query}),
+                        {$data_byline_field}
+                    ) > 0
+                    GROUP BY entry_id";
+        
+        $results = ee()->db->query($byline_query, $user_id)->result_array();
+
+        return $results;
     }
 }
